5086941f175442a49e31b6df555f7c1d520da15d
Bhuwan Yogi <yogibhuwan2004@gmail.com>
2026-05-04 12:04:13 +0530

    pyramid

---
+++ Day 4/pyramidStar.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r;
+    cout<<"Enter the number of rows: ";
+    cin>>r;
+    for(int i = 1; i<=r; i++){
+        for(int j = 1; j <= r-i; j++){
+            cout<<" ";
+        }
+        for(int r=1; r<=i; r++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
