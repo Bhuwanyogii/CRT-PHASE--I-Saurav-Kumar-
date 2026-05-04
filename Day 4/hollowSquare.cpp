6fc5ee62435cdd6b8efe3102afe12dad50b1c0f1
Bhuwan Yogi <yogibhuwan2004@gmail.com>
2026-05-04 11:38:03 +0530

    update

--- Day 4/hollowSquare.cpp
+++ Day 4/hollowSquare.cpp
@@ -9,10 +9,10 @@ int main(){
     for(int i=1; i<=r; i++){
         for(int j=1; j<=c; j++){
             if(i==1 || i==r || j==1 || j==c){
-                cout<<"*";
+                cout<<"* ";
             }
             else{
-                cout<<" ";
+                cout<<"  ";
             }
         }
         cout<<endl;
