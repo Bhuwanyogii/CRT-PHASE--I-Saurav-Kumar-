e5c7adca68a0b9e42e2f4c6962ab763a1094275b
Bhuwan Yogi <yogibhuwan2004@gmail.com>
2026-05-05 09:53:01 +0530

    new Codes

---
+++ Day 5/HelloWorldFunction.cpp
@@ -0,0 +1,10 @@
+// // function without return type and without argument
+#include<iostream>
+using namespace std;
+void printmsg(){
+    cout<<"Hello World";
+}
+int main(){
+    printmsg();
+    return 0;
+}
