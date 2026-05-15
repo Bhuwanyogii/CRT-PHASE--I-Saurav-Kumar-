881a4f5dd6dc29c24aaaf57d00855d1f5ab0272c
Bhuwan yogi <bhuwanyogi@gmail.com>
2026-05-15 10:47:51 +0530

    codes

--- Day 14/stackUsingArr.cpp
+++ Day 14/stackUsingArr.cpp
@@ -1,19 +1,65 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
 using namespace std;
+class Stack
+{
+private:
+    int top;
+    int arr[100];
+
+public:
+    Stack()
+    {
+        top = -1;
+    }
+    void push(int x)
+    {
+        if (top < 99)
+        {
+            top++;
+            arr[top] = x;
+        }
+        else
+        {
+            cout << "Stack Overflow" << endl;
+        }
+    }
+    void pop()
+    {
+        if (top >= 0)
+        {
+            top--;
+        }
+        else
+        {
+            cout << "Stack Underflow" << endl;
+        }
+    }
+
+    int peek()
+    {
+        if (top >= 0)
+        {
+            return arr[top];
+        }
+        else
+        {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+    }
+};
+
 int main()
 {
-    stack<int> s;
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    cout<<s.top()<<endl; 
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << "Top element: " << s.peek() << endl;
+
     s.pop();
-    cout<<s.top()<<endl; 
-    cout<<s.size()<<endl; 
-    cout<<s.empty()<<endl; 
-    s.pop();
-    s.pop();
-    cout<<s.empty()<<endl; 
+    cout << "Top element after pop: " << s.peek() << endl;
+
     return 0;
 }
